ae329751f64cfacf5b2e4f0ebbd66e5dbba4a21f
taivop <taivo.pungas@gmail.com>
2014-05-15 14:05:40 +0300

    Created CodeRunner dummy class.

---
+++ src/language/CodeRunner.py
@@ -0,0 +1,15 @@
+
+# This class should receive a list of statements, evaluate them and return an appropriate response.
+
+
+class CodeRunner:
+
+    def __init__(self):
+        # constructor
+        foo = 0 # placeholder
+
+
+    def execute_statements(self, list_of_statements):
+        foo = 0 # placeholder
+
+        return "does not compute"
