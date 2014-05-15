b664b4083ff4d69c55ef40c024af829011a38a52
taivop <taivo.pungas@gmail.com>
2014-05-15 14:42:06 +0300

    Start triangle.

--- src/gui/klotsid.py
+++ src/gui/klotsid.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Block(pygame.sprite.Sprite): # Something we can create and manipulate
-    def __init__(self,color,pos,size): # initialze the properties of the object
+    def __init__(self,color,pos,size): # initialize the properties of the object
         pygame.sprite.Sprite.__init__(self)
         self.color=color
         self.pos=pos
@@ -10,6 +10,23 @@ class Block(pygame.sprite.Sprite): # Something we can create and manipulate
     def Render(self,screen):
         blockimg = pygame.image.load("../resources/blockimg.png")
         screen.blit(blockimg,(self.pos))
+
+class StartTriangle():
+    def __init__(self,color,top_left_pos,width,height):
+        pygame.sprite.Sprite.__init__(self)
+        self.color = color
+        self.x = top_left_pos[0]
+        self.y = top_left_pos[1]
+        self.height = height
+        self.width = width
+
+    def Render(self,screen):
+        point_top_left = [self.x, self.y]
+        point_top_right = [self.x + self.width, self.y]
+        point_bottom = [self.x + self.width / 2, self.y + self.height]
+        points = [point_top_left, point_top_right,point_bottom]
+        pygame.draw.polygon(screen, self.color, points, 0)
+
 
 def main(): # Where we start
     screen=pygame.display.set_mode((600,400))
@@ -37,11 +54,13 @@ def main(): # Where we start
              
         if MousePressed==True:
             for item in RenderList: # search all items
-                if (pos[0]>=(item.pos[0]-item.size) and 
-                    pos[0]<=(item.pos[0]+item.size) and 
-                    pos[1]>=(item.pos[1]-item.size) and 
-                    pos[1]<=(item.pos[1]+item.size) ): # inside the bounding box
-                    Target=item # "pick up" item
+                #print(item.__class__.__name__)
+                if item.__class__.__name__ == 'Block':
+                    if (pos[0]>=(item.pos[0]-item.size) and
+                        pos[0]<=(item.pos[0]+item.size) and
+                        pos[1]>=(item.pos[1]-item.size) and
+                        pos[1]<=(item.pos[1]+item.size) ): # inside the bounding box
+                        Target=item # "pick up" item
             
             if Target is None: # didn't find any?
                 Target=Block((0,0,255),pos,50) # create a new one
@@ -52,7 +71,10 @@ def main(): # Where we start
         
         if MouseReleased:
             Target=None # Drop item, if we have any
-            
+
+        triangle=StartTriangle((0,255,0),[0,0], 40,15) # create a new one
+        RenderList.append(triangle) # add to list of things to draw
+
         for item in RenderList:
             item.Render(screen) # Draw all items
             
